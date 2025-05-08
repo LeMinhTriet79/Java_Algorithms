42017b183297ff30ff3e5f14f724767172091ef1
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-05-08 10:50:59 +0700

    Update MergeStringsAlternately.java

--- Array_and_String/MergeStringsAlternately/src/mergestringsalternately/MergeStringsAlternately.java
+++ Array_and_String/MergeStringsAlternately/src/mergestringsalternately/MergeStringsAlternately.java
@@ -9,27 +9,34 @@ package mergestringsalternately;
  * @author Minh Triet
  */
 public class MergeStringsAlternately {
-    public static String mergeAlternately (String word1, String word2){
+
+    public static String mergeAlternately(String word1, String word2) {
         //Khởi tạo StringBuilder
         StringBuilder result = new StringBuilder();
         //xem độ dài chuỗi nào dài hơn
         int n = Math.max(word1.length(), word2.length());
         //Duyệt từ 0 đến độ dài của chuỗi dài hơn
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             //Thêm kí tự tại vị trí "i" vào result
-            if(i < word1.length()){
+            if (i < word1.length()) {
                 result.append(word1.charAt(i));
             }
-            if(i < word2.length()){
+            if (i < word2.length()) {
                 result.append(word2.charAt(i));
             }
         }
         return result.toString();
     }
+
     public static void main(String[] args) {
         String a = "a b c"; //tạo ra các khoảng trắng để mô tả sự hợp nhất, sau đó bỏ khoảng trắng
         String b = " d e f";
         System.out.println("Output: " + mergeAlternately(a, b).replaceAll("\\s+", ""));
+
+        String c = "ab";
+        String d = "pqrs";
+        System.out.println("Output 2: " + mergeAlternately(c, d).replaceAll("\\s+", ""));
+
     }
-    
+
 }
