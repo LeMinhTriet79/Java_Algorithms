bb60e8576c4971ffa4910675b88461d7ea8ee77e
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-05-17 13:12:17 +0700

    up

---
+++ Array_and_String/605_CanPlaceFlowers/src/pkg605_canplaceflowers/Main.java
@@ -0,0 +1,40 @@
+package pkg605_canplaceflowers;
+
+/**
+ *
+ * @author Minh Triet
+ */
+public class Main {
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        //số lượng hoa đã trồng
+        int count = 0;
+
+        for (int i = 0; i < flowerbed.length; i++) {
+            // Kiểm tra vị trí HIỆN TẠI( i = 0 ) có trống và các vị trí xung quanh đều trống
+            if (flowerbed[i] == 0) {
+                //Nếu ở đầu mảng hoặc phía bên trái trống
+                boolean emptyLeft = (i == 0) || (flowerbed[i - 1] == 0);
+                //Nếu ở cuối mảng hoặc phía bên phải trống
+                boolean emptyRight = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
+
+                if (emptyLeft && emptyRight) {
+                    flowerbed[i] = 1; // Trồng hoa tại vị trí i
+                    count++;        // Tăng số lượng hoa đã trồng
+
+                    // Nếu đã trồng đủ số lượng cần thiết, trả về true
+                    if (count >= n) {
+                        return true;
+                    }
+                }
+            }
+
+        }
+        // Nếu sau khi duyệt hết mảng mà chưa trồng đủ số lượng thì trả về false
+        return count >= n;
+    }
+    public static void main(String[] args) {
+        int [] a = {1, 2, 3, 4, 5};
+        System.out.println(a.length);
+    }
+    
+}
